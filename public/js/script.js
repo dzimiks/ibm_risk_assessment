c02c82ae6f974881cb55f9180bbfa3c981390bde
dzimiks <vana997@gmail.com>
2019-09-08 01:10:34 +0200

    Add ID check feature and dynamic filter

--- public/js/script.js
+++ public/js/script.js
@@ -67,14 +67,14 @@ $(document).ready(function () {
     });
 
     //toggle sidebar overlay
-    $(".overlay").click(function () {
+    $(".overlay").click(() => {
         $(".page-wrapper").toggleClass("toggled");
     });
 
     //switch between themes
     let themes = "default-theme legacy-theme chiller-theme ice-theme cool-theme light-theme";
 
-    $('[data-theme]').click(function () {
+    $('[data-theme]').click(() => {
         $('[data-theme]').removeClass("selected");
         $(this).addClass("selected");
         $('.page-wrapper').removeClass(themes);
@@ -84,7 +84,7 @@ $(document).ready(function () {
     // switch between background images
     let bgs = "bg1 bg2 bg3 bg4";
 
-    $('[data-bg]').click(function () {
+    $('[data-bg]').click(() => {
         $('[data-bg]').removeClass("selected");
         $(this).addClass("selected");
         $('.page-wrapper').removeClass(bgs);
@@ -92,13 +92,13 @@ $(document).ready(function () {
     });
 
     // toggle background image
-    $("#toggle-bg").change(function (e) {
+    $("#toggle-bg").change((e) => {
         e.preventDefault();
         $('.page-wrapper').toggleClass("sidebar-bg");
     });
 
     // toggle border radius
-    $("#toggle-border-radius").change(function (e) {
+    $("#toggle-border-radius").change((e) => {
         e.preventDefault();
         $('.page-wrapper').toggleClass("boder-radius-on");
     });
@@ -116,4 +116,66 @@ $(document).ready(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="popover"]').popover();
+
+    $('#btnKupacID').click((e) => {
+        e.preventDefault();
+
+        // const kibanaOld = "https://47a7e36d434c46a284cd966d42828df9.eu-central-1.aws.cloud.es.io:9243/app/kibana#/dashboard/fc2d7b20-d187-11e9-9cc0-61448e3fa8be?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:17,i:'1',w:21,x:0,y:15),id:e7d99fa0-d187-11e9-9cc0-61448e3fa8be,panelIndex:'1',type:visualization,version:'7.3.1'),(embeddableConfig:(vis:(params:(sort:(columnIndex:0,direction:desc)))),gridData:(h:18,i:'2',w:24,x:24,y:32),id:eaed07e0-d18c-11e9-9cc0-61448e3fa8be,panelIndex:'2',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:18,i:'3',w:24,x:0,y:50),id:'3087a590-d191-11e9-9cc0-61448e3fa8be',panelIndex:'3',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:15,i:'4',w:48,x:0,y:0),id:bb2332a0-d191-11e9-9cc0-61448e3fa8be,panelIndex:'4',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:18,i:'5',w:24,x:0,y:32),id:'6c421dd0-d197-11e9-9cc0-61448e3fa8be',panelIndex:'5',type:visualization,version:'7.3.1'),(embeddableConfig:(vis:(defaultColors:('0%25+-+10%25':'rgb(247,251,255)','10%25+-+20%25':'rgb(227,238,249)','20%25+-+30%25':'rgb(208,225,242)','30%25+-+40%25':'rgb(182,212,233)','40%25+-+50%25':'rgb(148,196,223)','50%25+-+60%25':'rgb(107,174,214)','60%25+-+70%25':'rgb(74,152,201)','70%25+-+80%25':'rgb(46,126,188)','80%25+-+90%25':'rgb(23,100,171)','90%25+-+100%25':'rgb(8,74,145)'),legendOpen:!f)),gridData:(h:17,i:'6',w:27,x:21,y:15),id:'8ae3f690-d1a7-11e9-9cc0-61448e3fa8be',panelIndex:'6',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:18,i:'7',w:24,x:24,y:50),id:'9850af10-d1ae-11e9-9cc0-61448e3fa8be',panelIndex:'7',type:visualization,version:'7.3.1')),query:(language:kuery,query:''),timeRestore:!t,title:'Main+Dashboard',viewMode:view)";
+        const kupacID = $('#kupacID').val();
+        const changes = "('$state':(store:appState),meta:(alias:!n,controlledBy:'1567894826071',disabled:!f,index:b7a67e40-d1ad-11e9-9cc0-61448e3fa8be,key:KupacID,negate:!f,params:(query:" + kupacID + "),type:phrase,value:'" + kupacID.toLocaleString() + "'),query:(match:(KupacID:(query:" + kupacID + ",type:phrase))))";
+        const kibana = "https://47a7e36d434c46a284cd966d42828df9.eu-central-1.aws.cloud.es.io:9243/app/kibana#/dashboard/fc2d7b20-d187-11e9-9cc0-61448e3fa8be?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1y,to:now))&_a=(description:'',filters:!(" + changes + "),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:17,i:'1',w:21,x:0,y:15),id:e7d99fa0-d187-11e9-9cc0-61448e3fa8be,panelIndex:'1',type:visualization,version:'7.3.1'),(embeddableConfig:(vis:(params:(sort:(columnIndex:0,direction:desc)))),gridData:(h:18,i:'2',w:24,x:24,y:32),id:eaed07e0-d18c-11e9-9cc0-61448e3fa8be,panelIndex:'2',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:18,i:'3',w:24,x:0,y:50),id:'3087a590-d191-11e9-9cc0-61448e3fa8be',panelIndex:'3',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:15,i:'4',w:48,x:0,y:0),id:bb2332a0-d191-11e9-9cc0-61448e3fa8be,panelIndex:'4',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:18,i:'5',w:24,x:0,y:32),id:'6c421dd0-d197-11e9-9cc0-61448e3fa8be',panelIndex:'5',type:visualization,version:'7.3.1'),(embeddableConfig:(vis:(defaultColors:('0%25+-+10%25':'rgb(247,251,255)','10%25+-+20%25':'rgb(227,238,249)','20%25+-+30%25':'rgb(208,225,242)','30%25+-+40%25':'rgb(182,212,233)','40%25+-+50%25':'rgb(148,196,223)','50%25+-+60%25':'rgb(107,174,214)','60%25+-+70%25':'rgb(74,152,201)','70%25+-+80%25':'rgb(46,126,188)','80%25+-+90%25':'rgb(23,100,171)','90%25+-+100%25':'rgb(8,74,145)'),legendOpen:!f)),gridData:(h:17,i:'6',w:27,x:21,y:15),id:'8ae3f690-d1a7-11e9-9cc0-61448e3fa8be',panelIndex:'6',type:visualization,version:'7.3.1'),(embeddableConfig:(),gridData:(h:18,i:'7',w:24,x:24,y:50),id:'9850af10-d1ae-11e9-9cc0-61448e3fa8be',panelIndex:'7',type:visualization,version:'7.3.1')),query:(language:kuery,query:''),timeRestore:!t,title:'Main+Dashboard',viewMode:view)";
+
+        $('#kibanaMainDashboard').attr('src', kibana);
+        $('#kupacIDValue').text(kupacID);
+        $('#riskLevelDiv').toggleClass('d-none');
+
+        const riskLevelMoneyLabels = [500000, 2000000, 4000000, 6000000];
+        $('#riskLevelMoney').text(riskLevelMoneyLabels[Math.floor(Math.random() * riskLevelMoneyLabels.length)].toLocaleString());
+
+        const riskLevelLabels = ['No Risk', 'Low Risk', 'Medium Risk', 'High Risk'];
+        const riskLevel = $('#riskLevel').text(riskLevelLabels[Math.floor(Math.random() * riskLevelLabels.length)]);
+
+        switch (riskLevel[0].innerHTML) {
+            case 'No Risk':
+                $('#riskLevel').removeClass();
+                $('#riskLevel').addClass('d-inline');
+                $('#riskLevel').addClass('font-weight-bold');
+                $('#riskLevel').addClass('text-green');
+
+                $('#card-border-color').removeClass();
+                $('#card-border-color').addClass('card');
+                $('#card-border-color').addClass('border-top-green');
+                break;
+            case 'Low Risk':
+                $('#riskLevel').removeClass();
+                $('#riskLevel').addClass('d-inline');
+                $('#riskLevel').addClass('font-weight-bold');
+                $('#riskLevel').addClass('text-blue');
+
+                $('#card-border-color').removeClass();
+                $('#card-border-color').addClass('card');
+                $('#card-border-color').addClass('border-top-blue');
+                break;
+            case 'Medium Risk':
+                $('#riskLevel').removeClass();
+                $('#riskLevel').addClass('d-inline');
+                $('#riskLevel').addClass('font-weight-bold');
+                $('#riskLevel').addClass('text-orange');
+
+                $('#card-border-color').removeClass();
+                $('#card-border-color').addClass('card');
+                $('#card-border-color').addClass('border-top-orange');
+                break;
+            case 'High Risk':
+                $('#riskLevel').removeClass();
+                $('#riskLevel').addClass('d-inline');
+                $('#riskLevel').addClass('font-weight-bold');
+                $('#riskLevel').addClass('text-red');
+
+                $('#card-border-color').removeClass();
+                $('#card-border-color').addClass('card');
+                $('#card-border-color').addClass('border-top-red');
+                break;
+        }
+    });
 });
